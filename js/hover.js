453fa7ff316ff75ece6f762def002c4593e337a8
kknox1 <kknox1@swarthmore.edu>
2020-12-05 00:17:25 +0100

    hover prototype, all masterdoc edits up-to-date

--- js/hover.js
+++ js/hover.js
@@ -1,36 +1,52 @@
 
 (function (){
-    var li_cache, over = false;
-
-    $(".number")
-    .delegate(".number-definition", "mouseenter", function (e) {
-        var $li = $(this), speed;
-
-        if (li_cache === this && over) {
-            $.doTimeout("hoverOut");
-            return;
-        }
-
-        if (over) {
-            $.doTimeout("hoverOut", true);
-            speed = 0;
-        } else {
-            $.doTimeout("hoverOut");
-            speed = 500;
-        }
-
-        $.doTimeout("hoverIn", speed, function () {
-            over = true;
-            $li.find("div").fadeTo(200, 1.0);
-        });
-    })
-    .delegate(".number-definitions", "mouseleave", function (e) {
-        var $li = $(this);
-
-        $.doTimeout("hoverIn");
-        $.doTimeout("hoverOut", 500, function () {
-            over = false;
-            $li.find("div").stop(true).fadeOut();
-        });
+    $("figure").hoverIntent(function () {
+        $("figcaption", this).fadeTo(400, 1);
+    }, function () {
+        $("figcaption", this).fadeTo(400, 0);
     });
+
+    // Sample that worked: 
+    // <figure>
+    //     <img src="image.jpg" alt="cool image">
+    //         <figcaption>
+    //             <h3>Cool Image</h3>
+    //             <a href="http://coolplace.com">http://coolplace.com</a>
+    //         </figcaption>
+    // </figure>
+    
+
+    // var li_cache, over = false;
+
+    // $("figure")
+    //     .delegate("figcaption", "mouseenter", function (e) {
+    //         var $li = $(this), speed;
+
+    //         if (li_cache === this && over) {
+    //             $.doTimeout("hoverOut");
+    //             return;
+    //         }
+
+    //         if (over) {
+    //             $.doTimeout("hoverOut", true);
+    //             speed = 0;
+    //         } else {
+    //             $.doTimeout("hoverOut");
+    //             speed = 500;
+    //         }
+
+    //         $.doTimeout("hoverIn", speed, function () {
+    //             over = true;
+    //             $li.find("div").fadeTo(200, 1.0);
+    //         });
+    //     })
+    //     .delegate("figcaption", "mouseleave", function (e) {
+    //         var $li = $(this);
+
+    //         $.doTimeout("hoverIn");
+    //         $.doTimeout("hoverOut", 500, function () {
+    //             over = false;
+    //             $li.find("div").stop(true).fadeOut();
+    //         });
+    //     });
 })(jQuery); 
