7f00cebfcc8fbcc3996c3ae92bdaa065af142e20
kknox1 <kknox1@swarthmore.edu>
2021-01-06 00:49:13 -0500

    changes

--- js/new_map_interaction.js
+++ js/new_map_interaction.js
@@ -124,198 +124,35 @@
         $('#opportunity-gap').attr('style', 'display: block;')
         $('.state-name').html(`${state_name}`)
         $('.cover-heading').html(`${state_name}`)
-
-        state_data_iwt = {};
-        $.getJSON("/data/iwt_funding.json", function (data) {
-            state_data_iwt = data.find(d => {
-                return d.State === element_state_id
-            });
-            $('#iwt-dollars').html(`
-                    ${state_data_iwt['iwt_number']}
-                `);
-            $('#iwt-dollar-type').html(`
-                    Annual state funding for incumbent worker training in ${state_names[element_state_id]}. 
-                    <a target="_blank" href="https://www.nationalskillscoalition.org/resources/publications/file/Funding-Resilience-National-Skills-Coalition-digital-August-2020.pdf">
-                        Source
-                    </a>
-                `);
-        })
-        $.getJSON("../data/workforce_numbers.json", function (data) {
-            var state_data = data.find(d => {
-                return d.State === element_state_id
-            });
-            $('#dollars').html(`
-                    ${state_data['dollars']}
-                `);
-            $('#dollar-type').html(`
-                    Federal dislocated worker funding per unemployed person in ${state_names[element_state_id]}. 
-                `);
-            $('#demand').html(`
-                    ${state_data['demand']}
-                `);
-            $('#demand-type').html(`
-                    Open digital skills jobs. 
-                `);
-            $('#supply').html(`
-                    ${state_data['supply-PIACC']}
-                `);
-            $('#supply-type').html(`
-                    Unemployed lacking digital skills. 
-                `);
-            $('#EL').html(`
-                    ${state_data['Engaged Learners']}
-                `);
-            $('#LP').html(`
-                    ${state_data['LPs complete']}
-                `);
-        });
-        $.getJSON('/data/digital_skills.json', function (data){
-            var full_rows = []
-            var full_heading = '';
-
-            var state_data = data.find(d => {
-                return d.State === element_state_id
-            })
-
-            // For all indicators except IWT funds
-            for(var i = 0; i < number_of_indicators-2; i++){
-                var data_type = type_mappings[i]
-                var link_type = link_mappings[i]
-                var note = state_data[data_type].split(";");
-                var full_notes = []
-                var full_links = []
-
-                if (note[0].toUpperCase() == "YES") {
-                    full_heading = yes_headings[i]
-                } else {
-                    full_heading = no_headings[i]
-                }
-                $.each(note, function (j, n) {
-                    if (j > 0) {
-                        full_notes.push(`
-                                <p class="indicator-text">
-                                    ${n}
-                                </p>
-                            `)
-                    }
-                });
-                if (link_type in state_data) {
-                    var links = state_data[link_type].split(";");
-                    $.each(links, function (j, l) {
-                        if (j % 2 == 1) {
-                            full_links.push(`
-                                    <a class="indicator-link" href="${l}" target="_blank">${links[j - 1]}</a>,                                     
-                                `)
-                        };
-                    });
-                }
-                
-                full_rows.push(`
-                    </br></br>
-                    <h5 class="indicator-heading">${full_heading}<h5>
-                    ${full_notes.join("")}
-                    <span class="indicator-text">More resources: </span>${full_links.join("")}
-                `)
-            }
-
-            // IWT hard-coded :/
-            var full_notes_iwt = []
-            var full_links_iwt = []
-            var full_rows_iwt = []
-            var yn_val = state_data_iwt['iwt_YN']
-            var data_type = type_mappings[7]
-            var link_type = link_mappings[7]
-            var note_iwt = state_data_iwt[data_type].split(";");
-
-            if (yn_val == "YES") {
-                full_heading = yes_headings[7]
-            } else {
-                full_heading = no_headings[7]
-            }
-            $.each(note_iwt, function (j, n) {
-                if (j > 0) {
-                    full_notes_iwt.push(`
-                                <p class="indicator-text">
-                                    ${n}
-                                </p>
-                            `)
-                }
-            });
-            if (link_type in state_data_iwt) {
-                var links = state_data_iwt[link_type].split(";");
-                $.each(links, function (j, l) {
-                    if (j % 2 == 1) {
-                        full_links_iwt.push(`
-                                    <a class="indicator-link" href="${l}" target="_blank">${links[j - 1]}</a>,
-                                `)
-                    };
-                });
-            }
-            full_rows_iwt.push(`
-                    </br></br>
-                    <h5 class="indicator-heading">${full_heading}<h5>
-                    ${full_notes_iwt.join("")}
-                    <span class="indicator-text">More resources: </span>${full_links_iwt.join("")}
-                `)
-
-            // Additional Initiatives hard-coded :/ :/ not cool
-            var full_notes_9 = []
-            var full_links_9 = []
-            var full_rows_9 = []
-            var data_type = type_mappings[8]
-            var link_type = type_mappings[8]
-            var note_9 = state_data[data_type].split(";");
-
-            full_heading = `Additional Dedicated Digital Skills Activities`
-            $.each(note_9, function (j, n) {
-                if (j > 0) {
-                    full_notes_9.push(`
-                                <p class="indicator-text">
-                                    ${n}
-                                </p>
-                            `)
-                }
-            });
-            if (link_type in state_data) {
-                var links = state_data[link_type].split(";");
-                $.each(links, function (j, l) {
-                    if (j % 2 == 1) {
-                        full_links_9.push(`
-                                    <a class="indicator-link" href="${l}" target="_blank">${links[j - 1]}</a>
-                                `)
-                    };
-                });
-            }
-            full_rows_9.push(`
-                    </br></br>
-                    <h5 class="indicator-heading">${full_heading}<h5>
-                    ${full_notes_9.join("")}
-                    <span class="indicator-text">More resources: </span>${full_links_9.join("")}
-                `)
-            
-            $('#full_indicator_analysis').html(`
-                ${full_rows.join('')}
-                ${full_rows_iwt.join('')}
-                ${full_rows_9.join('')}
-            `)
-        })
+        
 
         $.getJSON("/data/skills_data_toy.json", function(data){
             var state_data = data.find(d => {
                 return d.State === element_state_id
             })
+
+            $('#supply').html(`${state_data['unemployed_without_ds']}`)
+            $('#demand').html(`${state_data['jobs_with_ds']}`)
+            $('#dollars').html(`${state_data['funding']}`)
+
+
             var rows = []
+            var full_rows = []
 
             for(var i = 1; i <= number_of_indicators; i++){
 
                 var YN_val = ''
                 var yn_type = `${i}_YN`;
                 var note_type = `${i}_note`;
+                var full_note_type = `${i}_full_note`;
+                var link_type = `${i}_links`;
                 
                 if(note_type in state_data){
                     var icon = ''; 
                     var heading = '';
                     var text = ''; 
+                    var full_text = [];
+                    var all_links = [];
                     if(yn_type in state_data){
                         if (state_data[yn_type].toUpperCase() == 'YES') {
                             // icon = `<img src="/img/success.png" style="width:2rem">`
@@ -349,6 +186,25 @@
                         text = `${state_data[note_type]}`
                     }
 
+                    var note = state_data[full_note_type].split(";");
+                    $.each(note, function (j, n) {
+                        full_text.push(`
+                            <p class="indicator-text">
+                                ${n}
+                            </p>
+                        `)
+                    })
+                    if (link_type in state_data) {
+                        var links = state_data[link_type].split(";");
+                        $.each(links, function (j, l) {
+                            if (j % 2 == 1) {
+                                all_links.push(`
+                                    <a class="indicator-link" href="${l}" target="_blank">${links[j - 1]}</a>,                                     
+                                `)
+                            };
+                        });
+                    }
+
                     rows.push(`
                         <tr>
                             <td>${icon}</td>
@@ -358,11 +214,21 @@
                             </td>
                         </tr>
                     `)
+                    full_rows.push(`
+                        </br></br>
+                        <h5 class="indicator-heading">${heading}<h5>
+                            ${full_text.join("")}
+                        <span class="indicator-text">More resources: </span>
+                            ${all_links.join("")}
+                    `)
                 }
             }
 
             $(`#indicators`).html(`
                 ${rows.join("")}
+            `)
+            $('#full_indicator_analysis').html(`
+                ${full_rows.join('')}
             `)
         })
 
